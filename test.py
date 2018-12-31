f6099a1add85fe28068921bd35e1c339f7ee3325
Young-Seob Jeong <bytecell@sch.ac.kr>
2018-12-31 21:11:26 +0900

    test.py 에 주석 추가

--- test.py
+++ test.py
@@ -4,22 +4,27 @@ import logging
 import time
 import pdb
 
+# logger 생성
 logger = logging.getLogger('sm')
+# logging.INFO는 화면에 수행과정 출력, logging.ERROR는 에러메시지만 출력
 logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
 logger.addHandler(streamHandler)
 
+# 규칙을 파일로부터 읽어들임
 rl = rule_loader(logger=logger)
 rl.load('./timex3.rule')
 if not rl.generate_rules():
     exit()
-
+# 읽어들인 규칙을 화면에 표시
 rl.print_rules()
 
+# 규칙 '수행(실행)' 객체 생성
 rp = rule_process(rules=rl.get_rules(), logger=logger)
 
 while True:
     text = input()
+    # 빈 줄 입력시 종료
     if not text:
         break
     start_time = time.time()
