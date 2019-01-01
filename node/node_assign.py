70256468a52564b8f18d644a69b547b7c806eddc
Young-Seob Jeong <bytecell@sch.ac.kr>
2019-01-02 01:05:22 +0900

    쓸데없는 주석 삭제

--- node/node_assign.py
+++ node/node_assign.py
@@ -61,32 +61,3 @@ class node_assign(node):
             position = _position
 
         return extent, position, pass_fail, reserved
-        
-        
-        # 각 자식노드에 대하여,
-        #    결과물 얻고, 실패하면 pass_fail = False 후, 리턴
-        #          성공 시, 결과물 str에 추가
-        #                   (단, 그 자식노드가 refer이면 reserved 사용)
-        # 모든 자식노드가 성공 시, 결과용 변수의 내용으로 var에 추가
-        
-        
-        """
-        for var_name, var_value in self._attr['variables'].items():
-            if var_value and var_value[0] == '@':
-                if self.num_child() != 1:
-                    self._logger.error('No children for variable assignment using rule-ref')
-                    return extent, position, pass_fail, reserved
-                _extent, _position, _pass_fail, _reserved = \
-                        self._children[0].process(text, extent, position, var, add_extent)
-                if _pass_fail:
-                    extent = _extent
-                    position = _position
-                    reserved = _reserved
-                    var.add(var_name, reserved)
-                else:
-                    pass_fail = False
-            else:
-                var.add(var_name, var_value)
-        reserved = None
-        return extent, position, pass_fail, reserved
-        """
