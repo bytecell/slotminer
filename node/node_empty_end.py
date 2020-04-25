e855a3d20944735182a976fdfa3d7f586b83df64
Young-Seob Jeong <bytecell@sch.ac.kr>
2020-04-26 00:25:27 +0900

    empty_begin & empty_end nodes

--- node/node_empty_end.py
+++ node/node_empty_end.py
@@ -1,10 +1,10 @@
 from node.node import node
 import re
 
-class node_empty_begin(node):
+class node_empty_end(node):
     def __init__(self, logger):
         node.__init__(self, logger=logger)
-        self._type = 'EMPTY|BEGIN'
+        self._type = 'EMPTY|END'
         self._white_space = re.compile('\s').match
 
     def process(self, text, extent, position, var, add_extent=True):
@@ -15,12 +15,10 @@ class node_empty_begin(node):
             self._logger.error('empty_node must be terminal node')
             return extent, position, pass_fail, reserved
 
-        if position >= len(text):
-            pass_fail = False
-        elif position == 0:
+        if position + 1 >= len(text):
             pass_fail = True
         else:
-            tmp = self._white_space(text[position-1:position])
+            tmp = self._white_space(text[position+1:position+2])
             if tmp:
                 pass_fail = True
 
